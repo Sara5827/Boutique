322e106e0d7b70b4d5b508dfa4b6402a297a4f15
ouldjelloulsara@gmail.com <ouldjelloulsara@gmail.com>
2021-03-02 11:10:13 +0100

    fixed register error

--- Boutique/app/Http/Controllers/AuthController.php
+++ Boutique/app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -14,31 +15,31 @@ class AuthController extends Controller
              'password'=>'required|confirmed'
          ]);
  
-         $validatedData['password'] = bcrypt($request->password);
+        //  $validatedData['password'] = bcrypt($request->password);
  
          $user = User::create($validatedData);
  
-         $accessToken = $user->createToken('authToken')->accessToken;
+         $accessToken = $user->createToken('authToken')->access_token;
  
          return response(['user'=> $user, 'access_token'=> $accessToken]);
         
     }
  
  
-    public function login(Request $request)
-    {
-         $loginData = $request->validate([
-             'email' => 'email|required',
-             'password' => 'required'
-         ]);
+    // public function login(Request $request)
+    // {
+    //      $loginData = $request->validate([
+    //          'email' => 'email|required',
+    //          'password' => 'required'
+    //      ]);
         
-         if(!auth()->attempt($loginData)) {
-             return response(['message'=>'Invalid credentials']);
-         }
+    //      if(!auth()->attempt($loginData)) {
+    //          return response(['message'=>'Invalid credentials']);
+    //      }
  
-         $accessToken = auth()->user()->createToken('authToken')->accessToken;
+    //      $accessToken = auth()->user()->createToken('authToken')->accessToken;
  
-         return response(['user' => auth()->user(), 'access_token' => $accessToken]);
+    //      return response(['user' => auth()->user(), 'access_token' => $accessToken]);
  
-    }
+    // }
 }
--- Boutique/routes/api.php
+++ Boutique/routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::resource('customers', 'CustomerController')->except(['create', 'edit']);
-Route::post('/register', 'Api/AuthController@register');
+Route::post('/register', 'AuthController@register');
